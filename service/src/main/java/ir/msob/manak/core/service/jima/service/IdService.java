e18211f5712924ab3e3be3eb28b16d58f685b236
yaghob abdi <y.abdi@rashasoft.ir>
2025-10-05 16:52:08 +0330

    DataBase

--- service/src/main/java/ir/msob/manak/core/service/jima/service/IdService.java
+++ service/src/main/java/ir/msob/manak/core/service/jima/service/IdService.java
@@ -1,20 +1,19 @@
 package ir.msob.manak.core.service.jima.service;
 
 import ir.msob.jima.core.commons.id.BaseIdService;
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class IdService implements BaseIdService {
-
     @Override
     public String newId() {
-        // TODO
-        return null;
+        return new ObjectId().toHexString();
     }
 
     @Override
     public String of(String id) {
-        // TODO
-        return null;
+        return id;
     }
 }
