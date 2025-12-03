209d8524fc2d6d1b50069d50919c38c7fdddd9da
yaghob abdi <y.abdi@rashasoft.ir>
2025-12-03 11:08:14 +0330

    Before After Operation

--- model/src/main/java/ir/msob/manak/core/model/jima/operation/BeforeAfterDomainOperation.java
+++ model/src/main/java/ir/msob/manak/core/model/jima/operation/BeforeAfterDomainOperation.java
@@ -1,11 +1,11 @@
 package ir.msob.manak.core.model.jima.operation;
 
-import ir.msob.jima.core.commons.domain.BaseCriteria;
-import ir.msob.jima.core.commons.domain.BaseDto;
 import ir.msob.jima.core.commons.operation.BaseBeforeAfterDomainOperation;
+import ir.msob.manak.core.model.jima.domain.Criteria;
+import ir.msob.manak.core.model.jima.domain.Dto;
 import ir.msob.manak.core.model.jima.security.User;
 
-public interface BeforeAfterDomainOperation<DTO extends BaseDto<String>, C extends BaseCriteria<String>>
+public interface BeforeAfterDomainOperation<DTO extends Dto, C extends Criteria>
         extends BaseBeforeAfterDomainOperation<String, User, DTO, C> {
 
 }
